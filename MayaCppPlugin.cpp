1857346f25a00174dd6f758037c41f22281a6369
aizwellenstan <aizwellenstan@gmail.com>
2022-07-22 18:59:37 +0800

    Update MayaCppPlugin.cpp

--- MayaCppPlugin.cpp
+++ MayaCppPlugin.cpp
@@ -10,5 +10,10 @@ MStatus BatchConvertMocap::doIt(const MArgList& args)
 	char Hello[] = "\n Hello World\nMaya C++\n";
 	cout << Hello << endl;
 	MGlobal::displayInfo(Hello);
+	
+	// Open file
+	char fileName[] = "J:/test_project/work/progress/mcp/satou_test/rig/tianTuanMo_v005_facial.ma";
+	MStatus stat = MFileIO::open(fileName, "mayaAscii", true);
+	
 	return stat;
 }
